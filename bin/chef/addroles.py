8d2b66b49757b99e9162e6bc5a8c4197092ffbdf
xiaodongwang <xiaodongwang@huawei.com>
2014-02-04 18:31:43 -0800

    make install script rerun and add logrotate support.
    Change-Id: I84526351f9aa4882d498601ebe9681e622909516

--- bin/chef/addroles.py
+++ bin/chef/addroles.py
@@ -1,15 +1,29 @@
 #!/usr/bin/env python
 
+import logging
 import os
 import os.path
 
-rolelist = []
-role_dir = '/var/chef/roles'
-for item in os.listdir(role_dir):
-    f = os.path.join(role_dir, item)
-    rolelist.append(f)
+from compass.utils import flags
+from compass.utils import logsetting
 
-for role in rolelist:
-    cmd = "knife role from file %s" % role
-    os.system(cmd)
 
+flags.add('roles_dir',
+          help='chef roles directory',
+          default='/var/chef/roles')
+
+
+if __name__ == '__main__':
+    flags.init()
+    logsetting.init()
+    rolelist = []
+    roles_dir = flags.OPTIONS.roles_dir
+
+    for item in os.listdir(roles_dir):
+        role_file = os.path.join(roles_dir, item)
+        rolelist.append(role_file)
+
+    for role in rolelist:
+        logging.info('add role %s', role)
+        cmd = "knife role from file %s" % role
+        os.system(cmd)

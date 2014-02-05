50719e46686573df7dcde59708ae7f4bb045dda0
xiaodongwang <xiaodongwang@huawei.com>
2014-02-04 18:31:43 -0800

    make install script rerun and add logrotate support.
    Change-Id: I84526351f9aa4882d498601ebe9681e622909516

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
-let initial_run=0
-while [ $# -gt 0 ]; do
-    case "$1" in
-        -i|--init) let initial_run=1; shift ;;
-        *) shift ;;
-    esac
-done
-if [ $initial_run -eq 0 ]; then
+/opt/compass/bin/manage_db.py checkdb
+if [[ "$?" == "0" ]]; then
 /opt/compass/bin/manage_db.py clean_clusters
 fi
 /opt/compass/bin/manage_db.py createdb

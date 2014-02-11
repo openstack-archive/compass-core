9b1474a12ca435d51e12688eb858f5480bcfc567
xiaodongwang <xiaodongwang@huawei.com>
2014-02-10 23:31:36 -0800

    move backend deployment integration test to actions, make test logging to tmp file. make update an extra databag item from config.
    Change-Id: I4ba825d9046b9b8563215fed5a73097077460889

--- compass/utils/flags.py
+++ compass/utils/flags.py
@@ -7,17 +7,46 @@ import sys
 from optparse import OptionParser
 
 
-PARSER = OptionParser()
-OPTIONS = None
+class Flags(object):
+    PARSER = OptionParser()
+    PARSED_OPTIONS = None
+
+    def parse_args(self):
+        (options, argv) = Flags.PARSER.parse_args()
+        sys.argv = [sys.argv[0]] + argv
+        Flags.PARSED_OPTIONS = options
+
+    def __getattr__(self, name):
+        if Flags.PARSED_OPTIONS and hasattr(Flags.PARSED_OPTIONS, name):
+            return getattr(Flags.PARSED_OPTIONS, name)
+
+        for option in Flags.PARSER.option_list:
+            if option.dest == name:
+                return option.default
+
+        raise AttributeError('Option instance has no attribute %s' % name)
+
+    def __setattr__(self, name, value):
+        if Flags.PARSED_OPTIONS and hasattr(Flags.PARSED_OPTIONS, name):
+            setattr(Flags.PARSED_OPTIONS, name, value)
+            return
+
+        for option in Flags.PARSER.option_list:
+            if option.dest == name:
+                option.default = value
+                return
+
+        object.__setattr__(self, name, value)
+
+
+OPTIONS = Flags()
 
 
 def init():
     """Init flag parsing.
     """
     global OPTIONS
-    (options, argv) = PARSER.parse_args()
-    sys.argv = [sys.argv[0]] + argv
-    OPTIONS = options
+    OPTIONS.parse_args()
 
 
 def add(flagname, **kwargs):
@@ -26,7 +55,8 @@ def add(flagname, **kwargs):
     :param flagname: flag name declared in cmd as --<flagname>=...
     :type flagname: str
     """
-    PARSER.add_option('--%s' % flagname, dest=flagname, **kwargs)
+    Flags.PARSER.add_option('--%s' % flagname,
+                            dest=flagname, **kwargs)
 
 
 def add_bool(flagname, default=True, **kwargs):
@@ -37,9 +67,9 @@ def add_bool(flagname, default=True, **kwargs):
     :param default: default value
     :type default: bool
     """
-    PARSER.add_option('--%s' % flagname,
-                      dest=flagname, default=default,
-                      action="store_true", **kwargs)
-    PARSER.add_option('--no%s' % flagname,
-                      dest=flagname,
-                      action="store_false", **kwargs)
+    Flags.PARSER.add_option('--%s' % flagname,
+                            dest=flagname, default=default,
+                            action="store_true", **kwargs)
+    Flags.PARSER.add_option('--no%s' % flagname,
+                            dest=flagname,
+                            action="store_false", **kwargs)

00aa2a03bf1c230438130e08a9b6d0c0512339a6
grace.yu <xinyu@huawei.com>
2014-02-20 14:12:24 -0800

    Support import CSV and deploy by command
    Change-Id: I07390fdb802ea5ab6b7c737b872feb82d0ebb62b

---
+++ compass/tests/apicommand/run_server.py
@@ -0,0 +1,270 @@
+#!/usr/bin/env python
+from copy import deepcopy 
+import simplejson as json
+import sys
+
+from compass.api import app
+from compass.db import database
+from compass.db.model import Switch
+from compass.db.model import Machine
+from compass.db.model import Cluster
+from compass.db.model import ClusterState
+from compass.db.model import ClusterHost
+from compass.db.model import HostState
+from compass.db.model import Adapter
+from compass.db.model import Role
+from compass.db.model import SwitchConfig
+from compass.utils import util
+
+
+def setupDb():
+
+    CLUSTER_SECURITY_CONFIG = {
+        "security": {
+            "server_credentials": {
+                "username": "root",
+                "password": "root"},
+            "service_credentials": {
+                "username": "service",
+                "password": "admin"},
+            "console_credentials": {
+                "username": "console",
+                "password": "admin"}
+        }
+    }
+
+    CLUSTER_NETWORKING_CONFIG = {
+        "networking": {
+            "interfaces": {
+                "management": {
+                    "ip_start": "10.120.8.100",
+                    "ip_end": "10.120.8.200",
+                    "netmask": "255.255.255.0",
+                    "gateway": "",
+                    "nic": "eth0",
+                    "promisc": 1
+                },
+                "tenant": {
+                    "ip_start": "192.168.10.100",
+                    "ip_end": "192.168.10.200",
+                    "netmask": "255.255.255.0",
+                    "gateway": "",
+                    "nic": "eth1",
+                    "promisc": 0
+                },
+                "public": {
+                    "ip_start": "12.145.68.100",
+                    "ip_end": "12.145.68.200",
+                    "netmask": "255.255.255.0",
+                    "gateway": "",
+                    "nic": "eth2",
+                    "promisc": 0
+                },
+                "storage": {
+                    "ip_start": "172.29.8.100",
+                    "ip_end": "172.29.8.200",
+                    "netmask": "255.255.255.0",
+                    "gateway": "",
+                    "nic": "eth3",
+                    "promisc": 0
+                }
+            },
+            "global": {
+                "nameservers": "8.8.8.8",
+                "search_path": "ods.com",
+                "gateway": "192.168.1.1",
+                "proxy": "http://127.0.0.1:3128",
+                "ntp_server": "127.0.0.1"
+            }
+        }
+    }
+
+    CLUSTER_PARTITION_CONFIG = {
+        "partition": "/home 20%;/tmp 10%;/var 30%;"
+    }
+
+    CLUSTERHOST_CONFIG = {
+        "networking": {
+            "interfaces": {
+                "management": {
+                    "ip": "%s"
+                },
+                "tenant": {
+                    "ip": "%s"
+                }
+            }
+        },
+        "roles": ["base"]
+    }
+
+    print "Setting up DB ..."
+    with database.session() as session:
+            # populate switch_config
+            switch_config = SwitchConfig(ip='192.168.1.10', filter_port='1')
+            session.add(switch_config)
+
+            # populate role table
+            role = Role(name='compute', target_system='openstack')
+            session.add(role)
+
+            # Populate one adapter to DB
+            adapter = Adapter(name='Centos_openstack', os='Centos',
+                              target_system='openstack')
+            session.add(adapter)
+
+            #Populate switches info to DB
+            switches = [Switch(ip="192.168.2.1",
+                               credential={"version": "2c",
+                                           "community": "public"},
+                               vendor="huawei",
+                               state="under_monitoring"),
+                        Switch(ip="192.168.2.2",
+                               credential={"version": "2c",
+                                           "community": "public"},
+                               vendor="huawei",
+                               state="under_monitoring"),
+                        Switch(ip="192.168.2.3",
+                               credential={"version": "2c",
+                                           "community": "public"},
+                               vendor="huawei",
+                               state="under_monitoring"),
+                        Switch(ip="192.168.2.4",
+                               credential={"version": "2c",
+                                           "community": "public"},
+                               vendor="huawei",
+                               state="under_monitoring"),
+            ]
+            session.add_all(switches)
+
+            # Populate machines info to DB
+            machines = [
+                Machine(mac='00:0c:27:88:0c:a1', port='1', vlan='1',
+                        switch_id=1),
+                Machine(mac='00:0c:27:88:0c:a2', port='2', vlan='1',
+                        switch_id=1),
+                Machine(mac='00:0c:27:88:0c:a3', port='3', vlan='1',
+                        switch_id=1),
+                Machine(mac='00:0c:27:88:0c:b1', port='1', vlan='1',
+                        switch_id=2),
+                Machine(mac='00:0c:27:88:0c:b2', port='2', vlan='1',
+                        switch_id=2),
+                Machine(mac='00:0c:27:88:0c:b3', port='3', vlan='1',
+                        switch_id=2),
+                Machine(mac='00:0c:27:88:0c:c1', port='1', vlan='1',
+                        switch_id=3),
+                Machine(mac='00:0c:27:88:0c:c2', port='2', vlan='1',
+                        switch_id=3),
+                Machine(mac='00:0c:27:88:0c:c3', port='3', vlan='1',
+                        switch_id=3),
+                Machine(mac='00:0c:27:88:0c:d1', port='1', vlan='1',
+                        switch_id=4),
+                Machine(mac='00:0c:27:88:0c:d2', port='2', vlan='1',
+                        switch_id=4),
+            ]
+
+            session.add_all(machines)
+            # Popluate clusters into DB
+            """
+            a. cluster #1: a new machine will be added to it.
+            b. cluster #2: a failed machine needs to be re-deployed.
+            c. cluster #3: a new cluster with 3 hosts will be deployed.
+            """
+            clusters_networking_config = [
+                {"networking":
+                    {"interfaces": {"management": {"ip_start": "10.120.1.100",
+                                                   "ip_end": "10.120.1.200"},
+                                    "tenant": {"ip_start": "192.168.1.100",
+                                               "ip_end": "192.168.1.200"},
+                                    "public": {"ip_start": "12.145.1.100",
+                                               "ip_end": "12.145.1.200"},
+                                    "storage": {"ip_start": "172.29.1.100",
+                                                "ip_end": "172.29.1.200"}}}
+                                    },
+                {"networking":
+                    {"interfaces": {"management": {"ip_start": "10.120.2.100",
+                                                   "ip_end": "10.120.2.200"},
+                                    "tenant": {"ip_start": "192.168.2.100",
+                                               "ip_end": "192.168.2.200"},
+                                    "public": {"ip_start": "12.145.2.100",
+                                               "ip_end": "12.145.2.200"},
+                                    "storage": {"ip_start": "172.29.2.100",
+                                                "ip_end": "172.29.2.200"}}}}
+            ]
+            cluster_names = ['cluster_01', 'cluster_02']
+            for name, networking_config in zip(cluster_names,
+                                               clusters_networking_config):
+                nconfig = deepcopy(CLUSTER_NETWORKING_CONFIG)
+                util.merge_dict(nconfig, networking_config)
+                c = Cluster(name=name, adapter_id=1,
+                            security_config=json.dumps(CLUSTER_SECURITY_CONFIG['security']),
+                            networking_config=json.dumps(nconfig['networking']),
+                            partition_config=json.dumps(CLUSTER_PARTITION_CONFIG['partition']))
+                session.add(c)
+            # Populate hosts to each cluster
+            host_mips = ['10.120.1.100', '10.120.1.101', '10.120.1.102', 
+                         '10.120.2.100', '10.120.2.101', '10.120.2.102']
+            host_tips = ['192.168.1.100', '192.168.1.101', '192.168.1.102',
+                         '192.168.2.100', '192.168.2.101', '192.168.2.102']
+
+            hosts_config = []
+            for mip, tip in zip(host_mips, host_tips):
+                config = deepcopy(CLUSTERHOST_CONFIG)
+                config['networking']['interfaces']['management']['ip'] = mip
+                config['networking']['interfaces']['tenant']['ip'] = tip
+                hosts_config.append(json.dumps(config))
+
+            hosts = [
+                ClusterHost(hostname='host_01', machine_id=1, cluster_id=1,
+                            config_data=hosts_config[0]),
+                ClusterHost(hostname='host_02', machine_id=2, cluster_id=1,
+                            config_data=hosts_config[1]),
+                ClusterHost(hostname='host_03', machine_id=3, cluster_id=1,
+                            config_data=hosts_config[2]),
+                ClusterHost(hostname='host_01', machine_id=4, cluster_id=2,
+                            config_data=hosts_config[3]),
+                ClusterHost(hostname='host_02', machine_id=5, cluster_id=2,
+                            config_data=hosts_config[4]),
+                ClusterHost(hostname='host_03', machine_id=6, cluster_id=2,
+                            config_data=hosts_config[5])
+            ]
+            session.add_all(hosts)
+
+            # Populate cluster state and host state
+            cluster_states = [
+                ClusterState(id=1, state="READY", progress=1.0,
+                             message="Successfully!"),
+                ClusterState(id=2, state="ERROR", progress=0.5,
+                             message="Failed!")
+            ]
+            session.add_all(cluster_states)
+
+            host_states = [
+                HostState(id=1, state="READY", progress=1.0,
+                          message="Successfully!"),
+                HostState(id=2, state="READY", progress=1.0,
+                          message="Successfully!"),
+                HostState(id=3, state="READY", progress=1.0,
+                          message="Successfully!"),
+                HostState(id=4, state="ERROR", progress=0.5,
+                          message="Failed!"),
+                HostState(id=5, state="READY", progress=1.0,
+                          message="Successfully!"),
+                HostState(id=6, state="ERROR", progress=1.0,
+                          message="Failed!")
+            ]
+            session.add_all(host_states)
+
+if __name__ == '__main__':
+    db_url, port = sys.argv[1:]
+    print db_url
+    database.init(db_url)
+    database.create_db()
+
+    try:
+        setupDb()
+    except:
+        pass
+    print "Starting server ....."
+    print "port is ", port
+
+    app.run(use_reloader=False, host="0.0.0.0", port=port)
---
+++ compass/tests/apicommand/test_csvdeploy.py
@@ -0,0 +1,127 @@
+import simplejson as json
+import os
+import sys
+import unittest2
+import tempfile
+import subprocess
+import shutil
+import signal
+import socket
+import time
+
+curr_dir = os.path.dirname(os.path.realpath(__file__))
+api_cmd_path = '/'.join((
+    os.path.dirname(os.path.dirname(os.path.dirname(curr_dir))), 'bin'))
+sys.path.append(api_cmd_path)
+import csvdeploy
+
+
+os.environ['COMPASS_IGNORE_SETTING'] = 'true'
+
+
+from compass.utils import setting_wrapper as setting
+reload(setting)
+
+
+from compass.db import database
+from compass.db.model import Cluster
+from compass.db.model import ClusterHost
+from compass.utils import flags
+from compass.utils import logsetting
+
+
+class ApiTestCase(unittest2.TestCase):
+    def setUp(self):
+        super(ApiTestCase, self).setUp()
+        # Create database file
+        try:
+            self.db_dir = tempfile.mkdtemp()
+            self.db_file = '/'.join((self.db_dir, 'app.db'))
+        except Exception:
+            sys.exit(2)
+
+        database_url = '/'.join(('sqlite://', self.db_file))
+        # Get a free random port for app server
+        try:
+            tmp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            tmp_socket.bind(('', 0))
+            self.port = tmp_socket.getsockname()[-1]
+            tmp_socket.close()
+            time.sleep(5)
+        except socket.error:
+            sys.exit(1)
+
+        # Start app server
+        cmd = 'python run_server.py %s %d' % (database_url, self.port)
+        self.proc = subprocess.Popen(cmd, shell=True,
+                                     stderr=subprocess.PIPE,
+                                     preexec_fn=os.setsid,
+                                     cwd=curr_dir)
+
+        time.sleep(5)
+        # Initial database
+        database.init(database_url)
+
+    def tearDown(self):
+        super(ApiTestCase, self).tearDown()
+
+        database.ENGINE.dispose()
+        os.killpg(self.proc.pid, signal.SIGTERM)
+        try:
+            if os.path.exists(self.db_dir):
+                shutil.rmtree(self.db_dir)
+        except Exception:
+            sys.exit(1)
+
+
+class TestAPICommand(ApiTestCase):
+    CSV_IMPORT_DIR = '/'.join((curr_dir, 'test_files'))
+
+    def setUp(self):
+        super(TestAPICommand, self).setUp()
+
+    def tearDown(self):
+        super(TestAPICommand, self).tearDown()
+
+    def test_start(self):
+        url = "http://localhost:%d" % self.port
+        csvdeploy.start(self.CSV_IMPORT_DIR, url)
+        clusters = csvdeploy.get_csv('cluster.csv',
+                                     csv_dir=self.CSV_IMPORT_DIR)
+        with database.session() as session:
+            for csv_cluster in clusters:
+                cluster_id = csv_cluster['id']
+                cluster = session.query(Cluster)\
+                                 .filter_by(id=cluster_id).first()
+                self.assertIsNotNone(cluster)
+                self.assertEqual(csv_cluster['name'], cluster.name)
+                self.assertDictEqual(csv_cluster['security_config'],
+                                     json.loads(cluster.security_config))
+                self.maxDiff = None
+                self.assertDictEqual(csv_cluster['networking_config'],
+                                     json.loads(cluster.networking_config))
+                self.assertEqual(csv_cluster['partition_config'],
+                                 json.loads(cluster.partition_config))
+                self.assertEqual(csv_cluster['adapter_id'], cluster.adapter_id)
+                self.maxDiff = None
+
+            hosts = csvdeploy.get_csv('cluster_host.csv',
+                                      csv_dir=self.CSV_IMPORT_DIR)
+            for csv_host in hosts:
+                cluster_id = csv_host['cluster_id']
+                hostname = csv_host['hostname']
+                host_in_db = session.query(ClusterHost)\
+                                    .filter_by(cluster_id=cluster_id,
+                                               hostname=hostname).first()
+                self.assertIsNotNone(host_in_db)
+                self.assertEqual(csv_host['hostname'], host_in_db.hostname)
+                self.assertEqual(csv_host['machine_id'], host_in_db.machine_id)
+                self.assertDictEqual(csv_host['config_data'],
+                                     json.loads(host_in_db.config_data))
+                self.maxDiff = None
+
+
+if __name__ == '__main__':
+    flags.init()
+    logsetting.init()
+    unittest2.main()

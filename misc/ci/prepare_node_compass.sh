8ce4382e6c6a4e8a0a414e8171f6a642d6df13c7
zhaoxinyu <zhaoxinyu@huawei.com>
2014-04-01 00:12:34 +0000

    Add tempest run scripts and prepare scripts
    Change-Id: I5dc1df89b32436a41cf7d7c46e30be879207b224

---
+++ misc/ci/prepare_node_compass.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+echo 0 > /selinux/enforce
+yum -y update
+sed -i "s/Defaults    requiretty/#Defaults    requiretty/"  /etc/sudoers
+git clone http://git.openstack.org/stackforge/compass-core
+cd compass-core
+source install/install.conf.template
+export tempest=true
+source install/dependency.sh
+source install/prepare.sh
+service libvirtd start
+sync
+sleep 5
+echo "image preparation done"

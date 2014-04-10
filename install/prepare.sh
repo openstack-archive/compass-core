bdffd5a89044580c986b50e9a0d6302fd6e4ddda
xiaodongwang <xiaodongwang@huawei.com>
2014-04-10 14:40:41 -0700

    make install script to catch up more typo error
    Change-Id: I4a45befe6c7f2d3205d70380b08d3881194f15a6

--- install/prepare.sh
+++ install/prepare.sh
@@ -1,53 +1,64 @@
 #!/bin/bash
 # prepare the installation
 
-copygit2dir()
-{
-    project=$1
-    destdir=$2
-    if [ -z "$REPO_URL" ];then
-        git_repo=http://git.openstack.org/stackforge/$project
-        gerrit_repo=https://review.openstack.org/stackforge/$project
-    else
-        git_repo=$REPO_URL/stackforge/$project
-        gerrit_repo=$REPO_URL/stackforge/$project
-    fi
-    if [ -d $destdir ];then
-        echo "$destdir exists"
-        cd $destdir
-        git remote set-url origin $git_repo
-        git remote update
-        git reset --hard
-        git clean -x -f
-        git checkout master
-        git reset --hard remotes/origin/master
-        if [[ -n "$GERRIT_REFSPEC" ]];then
-            git fetch $gerrit_repo $GERRIT_REFSPEC && git checkout FETCH_HEAD
-        fi
-        git clean -x -f
-    else
-        echo "create $destdir"
-        mkdir -p $destdir
-        git clone $git_repo $destdir
-        if [[ -n "$GERRIT_REFSPEC" ]];then
-            # project=$(echo $repo|rev|cut -d '/' -f 1|rev)
-            cd $destdir
-            git fetch $gerrit_repo $GERRIT_REFSPEC && git checkout FETCH_HEAD
-        fi
-    fi
-    cd $SCRIPT_DIR
-}
-
-copylocal2dir()
+copy2dir()
 {
     repo=$1
     destdir=$2
-    if [ -d $destdir ];then
-        echo "$destdir exists"
+    if [[ "$repo" =~ https?:// ]]; then
+        if [[ -d $destdir || -L $destdir ]]; then
+            cd $destdir
+            git status &> /dev/null
+            if [ $? -ne 0 ]; then
+                echo "$destdir is not git repo"
+                rm -rf $destdir
+            fi
+            cd -
+        fi
+
+        if [[ -d $destdir || -L $destdir ]]; then
+            echo "$destdir exists"
+            cd $destdir
+            git remote set-url origin $repo
+            git remote update
+            git reset --hard
+            git clean -x -f
+            git checkout master
+            git reset --hard remotes/origin/master
+        else
+            echo "create $destdir"
+            mkdir -p $destdir
+            git clone $repo $destdir
+            if [ $? -ne 0 ]; then
+                echo "failed to git clone $repo $destdir"
+                exit 1
+            fi
+            cd $destdir
+        fi
+        gerrit_repo=$3
+        if [ -z $gerrit_repo ]; then
+            if [[ -n "$GERRIT_REFSPEC" ]];then
+                git fetch $gerrit_repo $GERRIT_REFSPEC && git checkout FETCH_HEAD
+                if [ $? -ne 0 ]; then
+                    echo "failed to git fetch $gerrit_repo $GERRIT_REFSPEC"
+                    exit 1
+                fi
+            fi
+        fi
+        git clean -x -f
     else
-        mkdir -p $destdir
+        sudo rm -rf $destdir
+        sudo cp -rf $repo $destdir
+        if [ $? -ne 0 ]; then
+            echo "failed to copy $repo to $destdir"
+            exit 1
+        fi
     fi
-    sudo cp -rf $repo/* $destdir
+    if [[ ! -d $destdir && ! -L $destdir ]]; then
+        echo "$destdir doest not exist"
+        exit 1
+    fi
+    cd $SCRIPT_DIR
 }
 
 # Create backup dir
@@ -115,13 +126,16 @@ else
 fi
 
 cd $SCRIPT_DIR
-if [ "$source" != "local" ]; then
-  copygit2dir compass-web $WEB_HOME
-  copygit2dir compass-adapters $ADAPTER_HOME
-else 
-  copylocal2dir $WEB_SOURCE $WEB_HOME
-  copylocal2dir $ADAPTER_SOURCE $ADAPTER_HOME
+if [ -z $WEB_SOURCE ]; then
+    echo "web source $WEB_SOURCE is not set"
+    exit 1
 fi
+copy2dir $WEB_SOURCE $WEB_HOME $WEB_GERRIT_URL
+if [ -z $ADAPTERS_SOURCE ]; then
+    echo "adpaters source $ADAPTERS_SOURCE is not set"
+    exit 1
+fi
+copy2dir $ADAPTERS_SOURCE $ADAPTERS_HOME $ADAPTERS_GERRIT_URL
 
 if [ "$tempest" == "true" ]; then
     if [[ ! -e /tmp/tempest ]]; then
@@ -144,58 +158,48 @@ download()
 {
     url=$1
     package=${2:-$(basename $url)}
-    if [[ -f /tmp/${package} ]]; then
+    action=${3:-""}
+    if [[ -f /tmp/${package} || -L /tmp/${package} ]]; then
         echo "$package already exists"
     else
-        wget -c --progress=bar:force -O /tmp/${package}.tmp $url
+        if [[ "$url" =~ (http|https|ftp):// ]]; then
+            wget -c --progress=bar:force -O /tmp/${package}.tmp $url
+            if [[ "$?" != "0" ]]; then
+                echo "failed to download $package"
+                exit 1
+            else
+                echo "successfully download $package"
+                cp -rf /tmp/${package}.tmp /tmp/${package}
+            fi
+        else
+            cp -rf $url /tmp/${package}
+        fi
+        if [[ ! -f /tmp/${package} && ! -L /tmp/${package} ]]; then
+            echo "/tmp/$package is not created"
+            exit 1
+        fi
+    fi
+    if [[ "$action" == "install" ]]; then
+        sudo rpm -Uvh /tmp/$package
         if [[ "$?" != "0" ]]; then
-            echo "failed to download $package"
+            echo "failed to install $package"
             exit 1
         else
-            echo "successfully download $package"
-            cp -rf /tmp/${package}.tmp /tmp/${package}
+            echo "$package is installed"
         fi
+    elif [[ "$action" == "copy" ]]; then
+        destdir=$4
+        sudo cp /tmp/$package $destdir
+    elif [[ "$action" == "unzip" ]]; then
+        unzipped_package=${package%%.zip}
+        sudo rm -rf /tmp/$unzipped_package
+        sudo unzip -o /tmp/$package /tmp/
+        sudo cp -rf /tmp/$unzipped_package/. $destdir
     fi
 }
 
-# download chef-server package
-download $CHEF_SRV
-
-# download centos image
-download $IMAGE_SOURCE ${IMAGE_NAME}-${IMAGE_ARCH}.iso
-
-# download ppa_repo packages
-ppa_repo_packages="ntp-4.2.6p5-1.el6.${IMAGE_TYPE,,}.$IMAGE_ARCH.rpm 
-                   openssh-clients-5.3p1-94.el6.${IMAGE_ARCH}.rpm 
-                   iproute-2.6.32-31.el6.${IMAGE_ARCH}.rpm
-                   wget-1.12-1.8.el6.${IMAGE_ARCH}.rpm
-                   ntpdate-4.2.6p5-1.el6.${IMAGE_TYPE,,}.${IMAGE_ARCH}.rpm"
-for f in $ppa_repo_packages
-do
-    download ftp://rpmfind.net/linux/${IMAGE_TYPE,,}/${IMAGE_VERSION_MAJOR}/os/${IMAGE_ARCH}/Packages/$f $f
-done
-ppa_repo_rsyslog_packages="json-c-0.10-2.el6.$IMAGE_ARCH.rpm
-                           libestr-0.1.9-1.el6.$IMAGE_ARCH.rpm
-                           libgt-0.3.11-1.el6.$IMAGE_ARCH.rpm
-                           liblogging-1.0.4-1.el6.$IMAGE_ARCH.rpm
-                           rsyslog-7.6.3-1.el6.$IMAGE_ARCH.rpm"
-for f in $ppa_repo_rsyslog_packages
-do
-    download http://rpms.adiscon.com/v7-stable/epel-6/${IMAGE_ARCH}/RPMS/$f $f
-done
-
-# download chef client for ppa repo
-download http://opscode-omnibus-packages.s3.amazonaws.com/el/${IMAGE_VERSION_MAJOR}/${IMAGE_ARCH}/chef-11.8.0-1.el6.${IMAGE_ARCH}.rpm
-
 # download js mvc
-download http://github.com/downloads/bitovi/javascriptmvc/$JS_MVC.zip
-
-if [ -d /tmp/$JS_MVC ]; then
-echo "/tmp/$JS_MVC is already unzipped"
-else
-sudo unzip -o /tmp/$JS_MVC.zip -d /tmp/
-fi
-sudo cp -rf /tmp/$JS_MVC/. $WEB_HOME/public/
+download http://github.com/downloads/bitovi/javascriptmvc/$JS_MVC.zip $JS_MVC.zip unzip $WEB_HOME/public/
 
 # Install net-snmp
 sudo cp -rn /etc/snmp/snmp.conf /root/backup/
@@ -206,3 +210,9 @@ sudo cp -rf $COMPASSDIR/misc/snmp/snmp.conf /etc/snmp/snmp.conf
 sudo chmod 644 /etc/snmp/snmp.conf
 sudo mkdir -p /var/lib/net-snmp/mib_indexes
 sudo chmod 755 /var/lib/net-snmp/mib_indexes
+
+# generate ssh key
+if [ ! -e $HOME/.ssh/id_rsa.pub ]; then
+    rm -rf $HOME/.ssh/id_rsa
+    ssh-keygen -t rsa -f $HOME/.ssh/id_rsa -q -N ''
+fi

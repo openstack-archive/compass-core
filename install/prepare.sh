51d70e2a8f778614befc48e69c3bd42057bbdf99
zhaoxinyu <zhaoxinyu@huawei.com>
2014-04-16 00:24:32 +0000

    Add network connectivity test
    also add a skip-broken flag to yum update for the time-being
    because a syslinux dependency: syslinux-nonlinux was not found
    in centos mirrors. It is on the way though.
    https://www.centos.org/forums/viewtopic.php?f=14&t=45933
    Need 'yum clean all' to update metadata and get the latest package
    list from mirros.
    Fix a bug in prepare.sh. it didn't fetch the gerrit refs
    
    Change-Id: I66f06bdfb801204fb956c7a264c98de879322744

--- install/prepare.sh
+++ install/prepare.sh
@@ -36,7 +36,7 @@ copy2dir()
             cd $destdir
         fi
         gerrit_repo=$3
-        if [ -z $gerrit_repo ]; then
+        if [[ ! -z $gerrit_repo ]]; then
             if [[ -n "$GERRIT_REFSPEC" ]];then
                 git fetch $gerrit_repo $GERRIT_REFSPEC && git checkout FETCH_HEAD
                 if [ $? -ne 0 ]; then
@@ -130,12 +130,12 @@ if [ -z $WEB_SOURCE ]; then
     echo "web source $WEB_SOURCE is not set"
     exit 1
 fi
-copy2dir $WEB_SOURCE $WEB_HOME $WEB_GERRIT_URL
+copy2dir "$WEB_SOURCE" "$WEB_HOME" "$WEB_GERRIT_URL"
 if [ -z $ADAPTERS_SOURCE ]; then
     echo "adpaters source $ADAPTERS_SOURCE is not set"
     exit 1
 fi
-copy2dir $ADAPTERS_SOURCE $ADAPTERS_HOME $ADAPTERS_GERRIT_URL
+copy2dir "$ADAPTERS_SOURCE" "$ADAPTERS_HOME" "$ADAPTERS_GERRIT_URL"
 
 if [ "$tempest" == "true" ]; then
     if [[ ! -e /tmp/tempest ]]; then

a3cf07d06bbe92edddc06bd196d1a3046b44d160
zhaoxinyu <zhaoxinyu@huawei.com>
2014-04-16 00:24:32 +0000

    Add network connectivity test
    also add a skip-broken flag to yum update for the time-being
    because a syslinux dependency: syslinux-nonlinux was not found
    in centos mirrors. It is on the way though.
    https://www.centos.org/forums/viewtopic.php?f=14&t=45933
    
    Change-Id: I66f06bdfb801204fb956c7a264c98de879322744

--- install/dependency.sh
+++ install/dependency.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo 'Installing Required packages for Compass...'
-sudo yum update -y
+sudo yum update -y --skip-broken
 if [ "$tempest" == "true" ]; then
     sudo yum install -y virt-install libvirt qemu-kvm libxml2-devel libxslt-devel python-devel sshpass openssl-devel
 fi
-sudo yum install -y rsyslog logrotate ntp iproute openssh-clients python python-devel git wget python-setuptools python-netaddr python-flask python-flask-sqlalchemy python-amqplib amqp python-paramiko python-mock mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python python-daemon unzip openssl openssl098e ca-certificates redis python-redis
+sudo yum install -y rsyslog logrotate ntp iproute openssh-clients python python-devel git wget python-setuptools syslinux python-netaddr python-flask python-flask-sqlalchemy python-amqplib amqp python-paramiko python-mock mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python python-daemon unzip openssl openssl098e ca-certificates redis python-redis --skip-broken
 if [[ "$?" != "0" ]]; then
     echo "failed to install yum dependency"
     exit 1

149850e022461ca60d1c0cb5064b141f86968d9b
xiaodongwang <xiaodongwang@huawei.com>
2014-05-22 14:01:02 -0700

    change ubuntu image version to 14.04
    Change-Id: I3fcd37470199264284d61a947cb38828ff2ac4f3

--- install/dependency.sh
+++ install/dependency.sh
@@ -2,7 +2,7 @@
 
 echo 'Installing Required packages for Compass...'
 sudo yum clean all
-sudo yum update -y --skip-broken
+sudo yum update -y
 if [ "$tempest" == "true" ]; then
     sudo yum install -y virt-install libvirt qemu-kvm libxml2-devel libffi-devel libxslt-devel python-devel sshpass openssl-devel
     if [[ "$?" != "0" ]]; then
@@ -10,7 +10,7 @@ if [ "$tempest" == "true" ]; then
         exit 1
     fi
 fi
-sudo yum install -y rsyslog logrotate ntp iproute openssh-clients python python-devel git wget python-setuptools syslinux python-netaddr python-flask python-flask-sqlalchemy python-amqplib python-argparse amqp python-paramiko python-mock mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python python-daemon unzip openssl openssl098e ca-certificates redis python-redis --skip-broken
+sudo yum install -y rsyslog logrotate ntp iproute openssh-clients python python-devel git wget python-setuptools syslinux python-netaddr python-flask python-flask-sqlalchemy python-amqplib python-argparse amqp python-paramiko python-mock mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python python-daemon unzip openssl openssl098e ca-certificates redis python-redis python-importlib
 if [[ "$?" != "0" ]]; then
     echo "failed to install yum dependency"
     exit 1
@@ -35,13 +35,19 @@ fi
 # pip install flask-sqlalchemy need to remove /usr/lib64/python2.6/site-packages/easy-install.pth 
 cp -n /usr/lib/python2.6/site-packages/easy-install* /usr/lib64/python2.6/site-packages/
 
-if [ "$tempest" == "true" ]; then
-    sudo pip install -U setuptools
-    if [[ "$?" != "0" ]]; then
-        echo "failed to install setuptools"
-        exit 1 
-    fi 
+sudo pip install -U setuptools
+if [[ "$?" != "0" ]]; then
+    echo "failed to install setuptools"
+    exit 1 
 fi
+
+# TODO: (fixme). setuptools should be installed twice. One is to uninstall distribute, the other is to upgrade setuptools.
+ sudo pip install -U setuptools
+if [[ "$?" != "0" ]]; then
+    echo "failed to install setuptools"
+    exit 1 
+fi
+
 sudo pip install -U -r $COMPASSDIR/requirements.txt
 if [[ "$?" != "0" ]]; then
     echo "failed to install compass requirement packages"

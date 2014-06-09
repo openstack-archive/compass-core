ac37e91f138bcf5c2c33712d38d7627e9d5fe942
xiaodongwang <xiaodongwang@huawei.com>
2014-06-09 15:02:36 -0700

    update switch model.
    Change-Id: I032d06ec661b61ca79f1b6f987af0d026987b541

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -3,6 +3,7 @@
 if [[ "$?" == "0" ]]; then
 /opt/compass/bin/manage_db.py clean_clusters
 fi
+/opt/compass/bin/manage_db.py dropdb &> /dev/null
 /opt/compass/bin/manage_db.py createdb
 /opt/compass/bin/manage_db.py sync_switch_configs 
 /opt/compass/bin/manage_db.py sync_from_installers
@@ -10,5 +11,6 @@ service httpd restart
 service rsyslog restart
 service redis restart
 redis-cli flushall
+service mysqld restart
 service compassd restart
 

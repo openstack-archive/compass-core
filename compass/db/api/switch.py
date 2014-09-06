a7a1fec019921a02b17e34aaa003413bf88c40e9
xiaodongwang <xiaodongwang@huawei.com>
2014-09-05 20:50:05 -0700

    change networks format from list to dict
    Change-Id: I765be481a057824b2705c55da6adfde97da97e15

--- compass/db/api/switch.py
+++ compass/db/api/switch.py
@@ -88,7 +88,7 @@ RESP_MACHINES_HOSTS_FIELDS = [
     'id', 'switch_id', 'switch_ip', 'machine_id', 'switch_machine_id',
     'port', 'vlans', 'mac',
     'ipmi_credentials', 'tag', 'location',
-    'name', 'os_name', 'clusters',
+    'name', 'hostname', 'os_name', 'clusters',
     'created_at', 'updated_at'
 ]
 RESP_CLUSTER_FIELDS = [

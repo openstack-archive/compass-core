b4c4fb27519b0c93235e72839bb4e4f761f03048
xiaodongwang <xiaodongwang@huawei.com>
2014-09-23 11:36:38 -0700

    add ubuntu os only regtest conf
    Change-Id: Id5e72cad18cc9b864336ecc1febae23aa19e7e36

--- regtest/regtest.sh
+++ regtest/regtest.sh
@@ -116,6 +116,9 @@ fi
 if [[ ! -L compass_logs ]]; then
     ln -s /var/log/compass compass_logs
 fi
+if [[ ! -L chef_logs ]]; then
+    ln -s /var/log/chef chef_logs
+fi
 CLIENT_SCRIPT=/opt/compass/bin/client.py
 if [[ "$CLEAN_OLD_DATA" == "0" || "$CLEAN_OLD_DATA" == "false" ]]; then
     echo "keep old deployment data"

853dc0bc9ce94f8b0179bff214b3d22c633a5db9
Lei Lei <poppylei812@gmail.com>
2014-12-10 15:58:07 -0800

    Test external contributor
    Change-Id: I3d1cbca42dfb3f1c4a627221f5f8640a605a1e5c

--- compass/tests/db/api/base.py
+++ compass/tests/db/api/base.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
+#this line is to test contribution
 import datetime
 import logging
 import os

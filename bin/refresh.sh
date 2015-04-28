9d2f34263c6c8a11922ef213508f414fa0ce90a2
xiaodongwang <xiaodongwang@huawei.com>
2015-04-28 14:33:30 -0700

    update cluster state to error when deployment fails
    Change-Id: I54fb25d6151679118cf648b5c2192d56f7d95957

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -1,15 +1,32 @@
 #!/bin/bash
 set -e
 service mysqld restart
+service mysqld status || exit $?
 /opt/compass/bin/manage_db.py createdb
 /opt/compass/bin/clean_installers.py --noasync
 /opt/compass/bin/clean_installation_logs.py
 rm -rf /var/ansible/run/*
 service httpd restart
+sleep 10
+service httpd status || exit $?
 service rsyslog restart
+sleep 10
+service rsyslog status || exit $?
 service redis restart
 sleep 10
+service redis status || exit $?
 redis-cli flushall
+service cobblerd restart
+sleep 10
+service cobblerd status || exit $?
+chef-server-ctl restart
+sleep 10
+chef-server-ctl status || exit $?
 service compass-celeryd restart
+sleep 10
+killall -9 celery
+service compass-celeryd status || exit $?
 service compass-progress-updated restart
+sleep 10
+service compass-progress-updated status || exit $?
 

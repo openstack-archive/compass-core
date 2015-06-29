6770f106c8037006624dc3d252b270ccbc28ae5f
Jerry Zhao <xyzjerry@gmail.com>
2015-06-29 15:07:50 -0700

    migrate to pbr
    Change-Id: Ib7f8c8eb8de6047f3777bee4e5ac72f29a9ff191

--- setup.py
+++ setup.py
@@ -15,84 +15,16 @@
 # limitations under the License.
 
 
-"""setup script."""
+import setuptools
+
+# In python < 2.7.4, a lazy loading of package `pbr` will break
+# setuptools if some other modules registered functions in `atexit`.
+# solution from: http://bugs.python.org/issue15881#msg170215
 try:
-    from setuptools import find_packages
+    import multiprocessing  # noqa
 except ImportError:
-    from ez_setup import use_setuptools
-    use_setuptools()
+    pass
 
-
-from setuptools.command.test import test as TestCommand
-from setuptools import setup
-
-
-import os
-import sys
-
-
-# This helps python setup.py test command to utilize tox
-# See the instruction at https://testrun.org/tox/latest/example/basic.html\
-#                 #integration-with-setuptools-distribute-test-commands
-
-class Tox(TestCommand):
-    """Tox to do the setup."""
-
-    def finalize_options(self):
-        TestCommand.finalize_options(self)
-        self.test_args = []
-        self.test_suite = True
-
-    def run_tests(self):
-        import tox
-        errno = tox.cmdline(self.test_args)
-        sys.exit(errno)
-
-
-INSTALL_REQUIRES_FILE = os.path.join(
-    os.path.dirname(__file__), 'requirements.txt')
-with open(INSTALL_REQUIRES_FILE, 'r') as requires_file:
-    REQUIREMENTS = [line.strip() for line in requires_file if line != '\n']
-
-DATA_FILES_DIR = os.path.join(
-    os.path.dirname(__file__), 'conf')
-DATA_FILES = []
-for parent_dir, sub_dirs, files in os.walk(DATA_FILES_DIR):
-    if files == []:
-        pass
-    for file in files:
-        DATA_FILES.append((parent_dir, [os.path.join(parent_dir, file)]))
-
-setup(
-    name='compass',
-    version='0.1.0',
-
-    # general info
-    description="""Compass: automation framework of
-                system deployment on baremetal""",
-    author='Compass Development Group',
-    author_email='dev@syscompass.org',
-    url='https://github.com/stackforge/compass-core',
-    download_url='',
-
-    # dependency
-    install_requires=REQUIREMENTS,
-    packages=find_packages(exclude=['compass.tests']),
-    include_package_data=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: System Administrators',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: POSIX :: Linux',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-    ],
-    # data
-    data_files=DATA_FILES,
-    # test,
-    tests_require=['tox'],
-    cmdclass={'test': Tox},
-)
+setuptools.setup(
+    setup_requires=['pbr'],
+    pbr=True)

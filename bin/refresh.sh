6a0633b9a59ae6d7178f804e7bc5a12b24b915f7
baigk <baiguoku@huawei.com>
2015-11-16 22:48:47 -0800

    Merge branch "https://github.com/baigk/compass-core.git into dev/opnfv
    Change-Id: Ie2be5756f0c425a5d40e3092f52f245709fccbf3
    Signed-off-by: baigk <baiguoku@huawei.com>

--- bin/refresh.sh
+++ bin/refresh.sh
@@ -7,25 +7,16 @@ service mysqld status || exit $?
 /opt/compass/bin/clean_installation_logs.py
 rm -rf /var/ansible/run/*
 service httpd restart
-sleep 10
 service httpd status || exit $?
 service rsyslog restart
-sleep 10
 service rsyslog status || exit $?
 service redis restart
-sleep 10
 service redis status || exit $?
 redis-cli flushall
 service cobblerd restart
-sleep 10
 service cobblerd status || exit $?
-chef-server-ctl restart
-sleep 10
-chef-server-ctl status || exit $?
 service compass-celeryd restart
-sleep 10
 service compass-celeryd status || exit $?
 service compass-progress-updated restart
-sleep 10
 service compass-progress-updated status || exit $?
 

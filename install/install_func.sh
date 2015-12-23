22fca26c9a82e8903f906fbe3b1b83063bab0c3c
baigk <baiguoku@huawei.com>
2015-12-23 10:41:51 +0800

    change the adapter repo from openstack to compass4nfv
    change the adapter repo from openstack to compass4nfv so that
    can deploy openstack
    
    Change-Id: Ib5353ab29a44a772b250e93074c51182eabda245
    Signed-off-by: baigk <baiguoku@huawei.com>

--- install/install_func.sh
+++ install/install_func.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
 #
+reset=`tput sgr0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+
+function print_info() {
+    echo -e "${green}$*${reset}"
+}
+
+function print_warn() {
+    echo -e "${yellow}$*${reset}"
+}
+
+function print_error() {
+    echo -e "${red}$*${reset}"
+}
+
+function print_progress() {
+    echo -en "${yellow}$*\r${reset}"
+}
 
 fastesturl()
 {
@@ -33,100 +53,53 @@ copy2dir()
 {
     repo=$1
     destdir=$2
-    git_project=$3
-    git_branch=master
-    if [  -n "$4" ]; then
-       git_branch=$4
-    fi
+
     echo "copy $repo branch $git_branch to $destdir"
     if [[ "$repo" =~ (git|http|https|ftp):// ]]; then
-        if [[ -d $destdir || -L $destdir ]]; then
+        if [[ -d $destdir/.git ]]; then
             cd $destdir
             git status &> /dev/null
             if [ $? -ne 0 ]; then
-                echo "$destdir is not git repo"
-		cd -
+                cd -
                 rm -rf $destdir
-            else
-                echo "$destdir is git repo"
-		cd -
             fi
         fi
 
-        if [[ -d $destdir || -L $destdir ]]; then
-            echo "$destdir exists"
+        if [[ -d $destdir/.git ]]; then
             cd $destdir
             git remote set-url origin $repo
             git remote update
             if [ $? -ne 0 ]; then
-                echo "failed to git remote update $repo in $destdir"
-		cd -
+                print_error "failed to git remote update $repo in $destdir"
+                cd -
                 exit 1
-            else
-                echo "git remote update $repo in $destdir succeeded"
             fi
             git reset --hard
             git clean -x -f
             git checkout $git_branch
             git reset --hard remotes/origin/$git_branch
-	    git clean -x -f -d -q
-	    cd -
+            git clean -x -f -d -q
+            cd -
         else
-            echo "create $destdir"
             mkdir -p $destdir
-            git clone $repo $destdir -b $git_branch
+            git clone $repo $destdir
             if [ $? -ne 0 ]; then
-                echo "failed to git clone $repo $destdir"
+                print_error "failed to git clone $repo $destdir"
                 exit 1
-            else
-                echo "git clone $repo $destdir suceeded"
             fi
+            cd -
         fi
-	cd $destdir
-        if [[ -z $ZUUL_PROJECT ]]; then
-	    echo "ZUUL_PROJECT is not set"
-	elif [[ -z $ZUUL_BRANCH ]]; then
-	    echo "ZUUL_BRANCH is not set"
-        elif [[ -z $ZUUL_REF ]]; then
-	    echo "ZUUL_REF is not set"
-        elif [[ "$ZUUL_PROJECT" != "$git_project" ]]; then
-	    echo "ZUUL_PROJECT $ZUUL_PROJECT is not equal to git_project $git_project"
-        elif [[ "$ZUUL_BRANCH" != "$git_branch" ]]; then
-	    echo "ZUUL_BRANCH $ZUUL_BRANCH is not equal git_branch $git_branch"
-	else
-            git_repo=$ZUUL_URL/$ZUUL_PROJECT
-            git_ref=$ZUUL_REF
-            git reset --hard remotes/origin/$git_branch
-            git fetch $git_repo $git_ref
-            if [ $? -ne 0 ]; then
-                echo "failed to git fetch $git_repo $git_ref"
-		cd -
-		exit 1
-            fi
-	    git merge FETCH_HEAD
-	    if [ $? -ne 0 ]; then
-		echo "failed to git merge $git_ref"
-		cd -
-		exit 1
-	    fi
-            git clean -x -f
-        fi
-	cd -
     else
         sudo rm -rf $destdir
         sudo cp -rf $repo $destdir
         if [ $? -ne 0 ]; then
-            echo "failed to copy $repo to $destdir"
+            print_error "failed to copy $repo to $destdir"
             exit 1
-        else
-            echo "copy $repo to $destdir succeeded"
         fi
     fi
-    if [[ ! -d $destdir && ! -L $destdir ]]; then
-        echo "$destdir does not exist"
-        exit 1
-    else
-        echo "$destdir is ready"
+
+    if [[ $repo == https://gerrit.opnfv.org/gerrit/compass4nfv ]]; then
+        cp $destdir/deploy/adapters/{ansible,cobbler} $destdir/ -rf
     fi
 }
 

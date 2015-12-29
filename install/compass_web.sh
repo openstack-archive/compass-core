6e4731090e7a0d205a0a964473bc56a83edc0c0e
baigk <baiguoku@huawei.com>
2015-12-29 21:37:38 +0800

    modify the iso and ppa source
    modify the iso and ppa source
    
    Change-Id: I91b4a73e6f4aea69bd083acbabbfe1d2c849d8fd
    Signed-off-by: baigk <baiguoku@huawei.com>

--- install/compass_web.sh
+++ install/compass_web.sh
@@ -22,36 +22,6 @@ rm -rf /var/www/compass_web/*
 sudo mkdir -p /var/www/compass_web/v2.5
 sudo cp -rf $WEB_HOME/v2.5/target/* /var/www/compass_web/v2.5/
 
-if [[ $LOCAL_REPO = "y" ]]; then
-    echo "setting up local repo"
-    download -u "${LOCAL_REPO_US}/gem_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/gem_repo.tar.gz" gem_repo.tar.gz unzip /var/www/compass_web/v2 || exit $?
-    download -u "${LOCAL_REPO_US}/cirros-0.3.2-x86_64-disk.img" -u "${LOCAL_REPO_HUAWEI}/cirros-0.3.2-x86_64-disk.img" cirros-0.3.2-x86_64-disk.img copy /var/www/compass_web/v2 || exit $?
-    if [[ $SUPPORT_CENTOS_6_5 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/yum.repos.d/centos/6.5
-	download -u "${LOCAL_REPO_US}/centos/6.5/centos_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/centos/6.5/centos_repo.tar.gz" CentOS-6.5-x86_64.tar.gz unzip /var/www/compass_web/v2/yum.repos.d/centos/6.5 || exit $?
-    fi
-    if [[ $SUPPORT_CENTOS_6_6 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/yum.repos.d/centos/6.6
-	download -u "${LOCAL_REPO_US}/centos/6.6/centos_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/centos/6.6/centos_repo.tar.gz" CentOS-6.6-x86_64.tar.gz unzip /var/www/compass_web/v2/yum.repos.d/centos/6.6 || exit $?
-    fi
-    if [[ $SUPPORT_CENTOS_7_0 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/yum.repos.d/centos/7.0
-	download -u "${LOCAL_REPO_US}/centos/7.0/centos_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/centos/7.0/centos_repo.tar.gz" CentOS-7.0-x86_64.tar.gz unzip /var/www/compass_web/v2/yum.repos.d/centos/7.0 || exit $?
-    fi
-    if [[ $SUPPORT_UBUNTU_12_04 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/apt.repos.d/ubuntu/12.04
-	download -u "${LOCAL_REPO_US}/ubuntu/12.04/ubuntu_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/ubuntu/12.04/ubuntu_repo.tar.gz" Ubuntu-12.04-x86_64.tar.gz unzip /var/www/compass_web/v2/apt.repos.d/ubuntu/12.04 || exit $?
-    fi
-    if [[ $SUPPORT_UBUNTU_14_04 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/apt.repos.d/ubuntu/14.04
-	download -u "${LOCAL_REPO_US}/ubuntu/14.04/ubuntu_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/ubuntu/14.04/ubuntu_repo.tar.gz" Ubuntu-14.04-x86_64.tar.gz unzip /var/www/compass_web/v2/apt.repos.d/ubuntu/14.04 || exit $?
-    fi
-    if [[ $SUPPORT_SLES_11SP3 = "y" ]]; then
-	mkdir -p /var/www/compass_web/v2/zypp.repos.d/sles/11sp3
-	download -u "${LOCAL_REPO_US}/sles/11sp3/sles_repo.tar.gz" -u "${LOCAL_REPO_HUAWEI}/sles/11sp3/sles_repo.tar.gz" sles-11sp3-x86_64.tar.gz unzip /var/www/compass_web/v2/zypp.repos.d/sles/11sp3 || exit $?
-    fi
-fi
-
 sudo systemctl restart httpd.service
 sleep 10
 

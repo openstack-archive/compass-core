d801bd6215bee13e0c66605dd3bdb94cddce1303
xiaodongwang991481 <xiaodongwang991481@gmail.com>
2016-01-17 20:53:03 -0800

    make the installation code compatible on centos6 and centos7
    Change-Id: I463d2d463d30d50eb915592cbeaec553b23f0bec

--- install/dependency.sh
+++ install/dependency.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
+if [ -z "$MYSQL" ]; then
+    sudo rpm -Uvh $MYSQL >& /dev/null
+    if [[ "$?" != "0" ]]; then
+	echo "failed to install mysql repo"
+        exit 1
+    fi
+fi
+
 echo 'Installing Required packages for Compass...'
 sudo yum clean all
 sudo yum update -y --skip-broken
 if [ "$tempest" == "true" ]; then
-    sudo yum install -y virt-install libvirt qemu-kvm libxml2-devel libffi-devel libxslt-devel python-devel sshpass openssl-devel
+    sudo yum install -y virt-install libvirt qemu-kvm libxml2-devel libffi-devel libxslt-devel python-devel sshpass openssl-devel bridge-utils
     if [[ "$?" != "0" ]]; then
         echo "failed to install tempest yum dependency"
         exit 1
     fi
 fi
 
-sudo yum install -y $MYSQL rsyslog logrotate ntp iproute openssh-clients python python-devel git wget syslinux amqp rabbitmq-server mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python unzip openssl openssl098e ca-certificates mysql-devel mysql-server mysql MySQL-python python-virtualenv python-setuptools python-pip bc libselinux-python
+sudo yum install -y rsyslog logrotate ntp iproute openssh-clients python python-devel git wget syslinux amqp rabbitmq-server mod_wsgi httpd squid dhcp bind rsync yum-utils xinetd tftp-server gcc net-snmp-utils net-snmp net-snmp-python unzip openssl openssl098e ca-certificates MySQL-python python-virtualenv python-setuptools python-pip bc libselinux-python
+if [[ "$?" != "0" ]]; then
+    echo "faile dot install yum dependency"
+    exit 1
+fi
+
+sudo yum list mysql
+if [[ "$?" == "0" ]]; then
+sudo yum install -y mysql-devel mysql-server mysql
+else
+sudo yum install -y mariadb mariadb-devel mariadb-libs
+fi
+
 sudo yum --setopt=tsflags=noscripts -y remove redis
 sudo yum --enablerepo=remi,remi-test install -y redis
 
@@ -40,12 +60,25 @@ if [[ "$?" != "0" ]]; then
     exit 1
 fi
 
-sudo pip install virtualenvwrapper
+sudo pip install --upgrade virtualenvwrapper
 if [[ "$?" != "0" ]]; then
     echo "failed to install virtualenvwrapper"
     exit 1
 fi
 
+if [[ "$USE_SYSTEMCTL" == "0" ]]; then
+sudo chkconfig httpd on
+sudo chkconfig squid on
+sudo chkconfig xinetd on
+sudo chkconfig dhcpd on
+sudo chkconfig named on
+sudo chkconfig sshd on
+sudo chkconfig rsyslog on
+sudo chkconfig ntpd on
+sudo chkconfig redis on
+sudo chkconfig mysqld on
+sudo chkconfig rabbitmq-server on
+else
 sudo systemctl enable httpd.service
 sudo systemctl enable squid.service
 sudo systemctl enable xinetd.service
@@ -57,3 +90,4 @@ sudo systemctl enable ntpd.service
 sudo systemctl enable redis.service
 sudo systemctl enable mysql.service
 sudo systemctl enable rabbitmq-server.service
+fi
